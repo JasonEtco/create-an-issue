e1e98f774b49b716d6a7829c36bfa27ec25d9667
Jason Etcovitch <jasonetco@github.com>
2018-11-09 15:36:44 -0800

    Move some code around

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ console.log('INDEX FILE LOADED')
 
 const Toolkit = require('actions-toolkit')
 const fm = require('front-matter')
-const tools = new Toolkit()
-
-// Get the file
-const file = tools.getFile(process.argv[2] || '.github/ISSUE_TEMPLATE.md')
-
-console.log('FILE FOUND', file)
-
-// Get an authenticated Octokit client
-const octokit = tools.createOctokit()
 
 (async () => {
+  const tools = new Toolkit()
+
+  // Get an authenticated Octokit client
+  const octokit = tools.createOctokit()
+
+  // Get the file
+  const file = tools.getFile(process.argv[2] || '.github/ISSUE_TEMPLATE.md')
+  console.log('FILE FOUND', file)
+
+
   // Grab the front matter as JSON
   const { attributes, body } = fm(file)
   console.log(attributes)
 
   // Create the new issue
   return octokit.issues.create(tools.context.repo({
-    title: attributes.title,
     body,
+    title: attributes.title,
     assignees: attributes.assignees || [],
-    labels: attributes.labels || []
+    labels: attributes.labels || [],
+    milestone: attributes.milestone
   }))
 })()
