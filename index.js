1a4f8a7badb578306988f29d465d87398adcd1ea
Jason Etcovitch <jasonetco@github.com>
2018-11-09 15:33:20 -0800

    Use front-matter lib

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 console.log('INDEX FILE LOADED')
 
 const Toolkit = require('actions-toolkit')
-const yaml = require('js-yaml')
+const fm = require('front-matter')
 const tools = new Toolkit()
-
-const FRONT_MATTER_REG = /^-{3}\n(?<frontmatter>[\s\S]+)\n-{3}\n(?<body>[\s\S]+)/
 
 // Get the file
 const file = tools.getFile(process.argv[2] || '.github/ISSUE_TEMPLATE.md')
-console.log(file)
 
 console.log('FILE FOUND', file)
 
@@ -16,20 +13,15 @@ console.log('FILE FOUND', file)
 const octokit = tools.createOctokit()
 
 (async () => {
-  // Parse out the front matter
-  const match = FRONT_MATTER_REG.exec(file)
-  console.log('MATCH IS', match)
-  if (!match) return
-
   // Grab the front matter as JSON
-  const fm = yaml.safeLoad(match.groups.frontmatter)
-  console.log(fm)
+  const { attributes, body } = fm(file)
+  console.log(attributes)
 
   // Create the new issue
   return octokit.issues.create(tools.context.repo({
-    title: fm.title,
-    body: match.groups.body,
-    assignees: fm.assignees || [],
-    labels: fm.labels || []
+    title: attributes.title,
+    body,
+    assignees: attributes.assignees || [],
+    labels: attributes.labels || []
   }))
 })()
