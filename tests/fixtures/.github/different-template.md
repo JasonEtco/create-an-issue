7609992e65cce39a71cb9158ece624648c2d7e50
Jason Etcovitch <jasonetco@github.com>
2018-11-09 17:51:41 -0800

    Add another test

---
+++ tests/fixtures/.github/different-template.md
@@ -0,0 +1,4 @@
+---
+title: Different file
+---
+Goodbye!
--- tests/index.test.js
+++ tests/index.test.js
@@ -18,4 +18,15 @@ describe('create-an-issue', () => {
     expect(github.issues.create).toHaveBeenCalled()
     expect(github.issues.create.mock.calls).toMatchSnapshot()
   })
+
+  it('creates a new issue from a different template', async () => {
+    issueCreator = new IssueCreator('.github/different-template.md')
+    issueCreator.tools.workspace = path.join(__dirname, 'fixtures')
+    issueCreator.tools.context.payload = { repository: { owner: { login: 'JasonEtco' }, name: 'waddup' } }
+    issueCreator.tools.createOctokit = jest.fn(() => github)
+
+    await issueCreator.go()
+    expect(github.issues.create).toHaveBeenCalled()
+    expect(github.issues.create.mock.calls[0][0].title).toBe('Different file')
+  })
 })
