e4b7522d0299e722239db5f5fb5580da1e791de8
Jason Etcovitch <jasonetco@github.com>
2019-02-17 12:38:22 -0500

    Success log

--- entrypoint.js
+++ entrypoint.js
@@ -5,5 +5,5 @@ const tools = new Toolkit()
 const issueCreator = new IssueCreator(tools)
 issueCreator.go()
   .then(issue => {
-    tools.log(`Created issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
+    tools.log.success(`Created issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
   })
