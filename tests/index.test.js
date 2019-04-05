eb52522a3035772bc94d76a07c596e77922c6498
Jason Etcovitch <jasonetco@github.com>
2019-04-05 14:35:22 -0400

    Use nock in tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,32 +1,29 @@
-const path = require('path')
+const nock = require('nock')
 const { Toolkit } = require('actions-toolkit')
 
 describe('create-an-issue', () => {
-  let actionFn, tools
+  let actionFn, tools, scoped
 
   beforeEach(() => {
     Toolkit.run = jest.fn(fn => { actionFn = fn })
     require('..')
 
-    Object.assign(process.env, {
-      GITHUB_EVENT_PATH: path.join(__dirname, 'fixtures', 'event.json'),
-      GITHUB_WORKSPACE: path.join(__dirname, 'fixtures')
+    scoped = nock('https://api.github.com')
+      .post(/\/repos\/.*\/.*\/issues/).reply(200, (_, body) => ({
+        title: body.title,
+        number: 1,
+        html_url: 'www'
+      }))
+
+    tools = new Toolkit({
+      logger: {
+        info: jest.fn(),
+        success: jest.fn(),
+        warn: jest.fn(),
+        debug: jest.fn()
+      }
     })
 
-    tools = new Toolkit({ logger: {
-      info: jest.fn(),
-      success: jest.fn(),
-      warn: jest.fn()
-    } })
-    tools.github = {
-      issues: {
-        create: jest.fn(({ title }) => Promise.resolve({ data: {
-          title,
-          number: 1,
-          html_url: 'www'
-        } }))
-      }
-    }
     tools.exit.success = jest.fn()
     tools.exit.failure = jest.fn()
   })
@@ -34,19 +31,16 @@ describe('create-an-issue', () => {
   it('creates a new issue', async () => {
     tools.log.success = jest.fn()
     await actionFn(tools)
-    expect(tools.github.issues.create).toHaveBeenCalled()
-    expect(tools.github.issues.create.mock.calls).toMatchSnapshot()
     expect(tools.log.success).toHaveBeenCalled()
     expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
 
   it('creates a new issue from a different template', async () => {
     tools.arguments._ = ['.github/different-template.md']
-    tools.workspace = path.join(__dirname, 'fixtures')
     tools.context.payload = { repository: { owner: { login: 'JasonEtco' }, name: 'waddup' } }
     await actionFn(tools)
-    expect(tools.github.issues.create).toHaveBeenCalled()
-    expect(tools.github.issues.create.mock.calls[0][0].title).toBe('Different file')
+    expect(tools.log.success).toHaveBeenCalled()
+    expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
 
   it('creates a new issue with some template variables', async () => {
@@ -59,7 +53,7 @@ describe('create-an-issue', () => {
   it('creates a new issue with assignees, labels and a milestone', async () => {
     tools.arguments._[0] = '.github/kitchen-sink.md'
     await actionFn(tools)
-    expect(tools.github.issues.create).toHaveBeenCalled()
-    expect(tools.github.issues.create.mock.calls).toMatchSnapshot()
+    expect(tools.log.success).toHaveBeenCalled()
+    expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
 })
