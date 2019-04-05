5613e9065a3762726d0dd514742759ecb40bf972
Jason Etcovitch <jasonetco@github.com>
2019-04-05 14:39:19 -0400

    Pass tests with nock

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,18 +2,21 @@ const nock = require('nock')
 const { Toolkit } = require('actions-toolkit')
 
 describe('create-an-issue', () => {
-  let actionFn, tools, scoped
+  let actionFn, tools, params
 
   beforeEach(() => {
     Toolkit.run = jest.fn(fn => { actionFn = fn })
     require('..')
 
-    scoped = nock('https://api.github.com')
-      .post(/\/repos\/.*\/.*\/issues/).reply(200, (_, body) => ({
-        title: body.title,
-        number: 1,
-        html_url: 'www'
-      }))
+    nock('https://api.github.com')
+      .post(/\/repos\/.*\/.*\/issues/).reply(200, (_, body) => {
+        params = JSON.parse(body)
+        return {
+          title: params.title,
+          number: 1,
+          html_url: 'www'
+        }
+      })
 
     tools = new Toolkit({
       logger: {
@@ -31,6 +34,7 @@ describe('create-an-issue', () => {
   it('creates a new issue', async () => {
     tools.log.success = jest.fn()
     await actionFn(tools)
+    expect(params).toMatchSnapshot()
     expect(tools.log.success).toHaveBeenCalled()
     expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
@@ -39,6 +43,7 @@ describe('create-an-issue', () => {
     tools.arguments._ = ['.github/different-template.md']
     tools.context.payload = { repository: { owner: { login: 'JasonEtco' }, name: 'waddup' } }
     await actionFn(tools)
+    expect(params).toMatchSnapshot()
     expect(tools.log.success).toHaveBeenCalled()
     expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
@@ -46,13 +51,14 @@ describe('create-an-issue', () => {
   it('creates a new issue with some template variables', async () => {
     tools.arguments._[0] = '.github/variables.md'
     await actionFn(tools)
-    expect(tools.github.issues.create).toHaveBeenCalled()
-    expect(tools.github.issues.create.mock.calls).toMatchSnapshot()
+    expect(tools.log.success).toHaveBeenCalled()
+    expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
 
   it('creates a new issue with assignees, labels and a milestone', async () => {
     tools.arguments._[0] = '.github/kitchen-sink.md'
     await actionFn(tools)
+    expect(params).toMatchSnapshot()
     expect(tools.log.success).toHaveBeenCalled()
     expect(tools.log.success.mock.calls).toMatchSnapshot()
   })
