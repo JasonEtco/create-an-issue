92349458cf40765f4b4b32dda91bc1db3944c220
Abhijeet-Pubba <59115226+Abhijeet-Pubba@users.noreply.github.com>
2023-06-26 13:14:30 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Toolkit } from "actions-toolkit";
 import { createAnIssue } from "./action";
 
+console.log(secrets: ["GITHUB_TOKEN"])
+
 Toolkit.run(createAnIssue, {
   secrets: ["GITHUB_TOKEN"],
 });
